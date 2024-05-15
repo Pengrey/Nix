8cf9649bbeffa714d0b8b5eb7368290df02e15f9
Pengrey <55480558+Pengrey@users.noreply.github.com>
2024-05-15 17:41:06 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,6 @@
-# NixOs configuration file
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
 
@@ -9,11 +11,15 @@
     ];
 
   # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = true;
-
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  
   networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -36,127 +42,102 @@
     LC_TIME = "pt_PT.UTF-8";
   };
 
-  # Set fonts
-  fonts.fonts = with pkgs; [
-    roboto
-    cascadia-code
-  ];
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
 
-  environment.pathsToLink = [ "/libexec" ]; # for i3
-  # Enable i3
-  services.xserver = {
-    enable = true;
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.wayland.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
-    # Setup Keyboard Layout
+  # Configure keymap in X11
+  services.xserver.xkb = {
     layout = "pt";
-    xkbOptions = "nodeadkeys";
+    variant = "nodeadkeys";
+  };
 
-    # Resolutions 1920x1080
-    resolutions = [
-    		    {
-		      x = 1920;
-		      y = 1080;
-		    }
-		  ];
+  # Configure console keymap
+  console.keyMap = "pt-latin1";
 
-    # i3 stuff
-    desktopManager = {
-      xterm.enable = false;
-      wallpaper.mode = "fill";
-    };
-   
-    displayManager.defaultSession = "none+i3";
+  # Enable CUPS to print documents.
+  #services.printing.enable = true;
 
-    windowManager.i3 = {
-      enable = true;
-      package = pkgs.i3-gaps;
-      extraPackages = with pkgs; [
-        dmenu #application launcher most people use
-        i3lock #default i3 screen locker
-	i3status
-	i3blocks
-     ];
-    };
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
-  
-  # Define the the shell to be used by the users
-  users.defaultUserShell = pkgs.zsh;
 
+  # Enable tailscale support
+  services.tailscale.enable = true;
+ 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.pengrey = {
+  users.users.user01 = {
     isNormalUser = true;
-    description = "pengrey";
+    description = "user01";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
-      firefox		# Internet browser
-      arandr		# xrandr gui
+      firefox
+      kate
     ];
   };
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile.
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    # System commands
-    exa			    # ls replacement
-    wget		    # File retriever
-    btop		    # Rsource Monitor
-    bat			    # cat replacement
-    neofetch		    # Shows system information
-    zathura		    # PDF viewer
-
-    # Window Manager Stuff
-    alacritty		        # GPU Accelerated Terminal Emulator
-    lf				# Terminal file maneger writen in go
-    nm-tray			# Network manager tray GUI
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    tailscale
+    vscode
+    mpv
   ];
- 
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    pulse.enable = true;
-  };
 
-  # NeoVim configuration
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;	# Set nvim as the default editor using EDITOR env var
-    vimAlias = true; 		  # Symlink vim to nvim
-  };
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
 
-  # ZSH Configuration with ohMysZsh
-  programs.zsh = {
-    enable = true;
-    autosuggestions.enable = true;
-    syntaxHighlighting.enable = true;
-    shellAliases = {
-      vim = "nvim";
-      ls = "exa -l";
-      cat = "bat";
-      update = "sudo nixos-rebuild switch";
-    };
-    ohMyZsh = {
-      enable = true;
-      theme = "gozilla";
-    };
-  };
+  # List services that you want to enable:
 
-  # Tmux Configuration
-  programs.tmux = {
-    enable = true;
-    extraConfig = ''
-      # Use Mouse Support
-      set-option -g mouse on
-    '';
-  };
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
-  # on your system were taken.
-  system.stateVersion = "22.11";
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
 
+  systemd.services.NetworkManager-wait-online = {
+    serviceConfig = {
+      ExecStart = [ "" "${pkgs.networkmanager}/bin/nm-online -q" ];
+    };
+  };
 }

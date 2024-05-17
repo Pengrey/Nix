1d8e56d246f41b302b8e47798b4ed499275226e8
Pengrey <55480558+Pengrey@users.noreply.github.com>
2024-05-17 10:45:53 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -77,6 +77,12 @@
     # no need to redefine it in your config for now)
     #media-session.enable = true;
   };
+  
+  # Enable bluetooth
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;

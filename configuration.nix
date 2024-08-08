5fd7e6153751ba875ceee9e58aa087caf743efd8
Pengrey <55480558+Pengrey@users.noreply.github.com>
2024-08-08 15:29:47 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -86,9 +86,6 @@
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
-
-  # Enable tailscale support
-  services.tailscale.enable = true;
  
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.user01 = {
@@ -109,8 +106,7 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
-    tailscale
-    vscode
+    zed-editor
     mpv
   ];
 
